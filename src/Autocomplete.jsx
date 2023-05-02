7d7b2c86e1ae6985a2ef26e238f6af036433d0f3
bakibilici <efurkanbilici@gmail.com>
2023-05-02 15:27:25 +0300

    feature:dark mode

--- src/Autocomplete.jsx
+++ src/Autocomplete.jsx
@@ -80,17 +80,17 @@ const AutoComplete = ({ data, keys = [] }) => {
 
   return (
     <div>
-      <div className="wrapper border">
+      <div className="wrapper border dark:border-slate-600">
         <label
           htmlFor="searchbox"
-          className="absolute top-0 left-0 w-10 h-full flex items-center justify-center select-none cursor-pointer"
+          className="absolute top-0 left-0 w-10 h-full flex items-center justify-center select-none cursor-pointer text-[#333] dark:text-[#888]"
         >
-          <FiSearch size={18} color="#333" />
+          <FiSearch size={18} />
         </label>
         <input
           type="search"
           placeholder="Find what you are looking for"
-          className="w-full h-full text-slate-800 pl-8"
+          className="w-full h-full text-slate-800 pl-8 dark:text-white/80 bg-transparent dark:placeholder:text-slate-400"
           autoFocus={true}
           id="searchbox"
           onChange={handleChange}
@@ -98,27 +98,27 @@ const AutoComplete = ({ data, keys = [] }) => {
       </div>
       <div className="mt-4 text-center">
         {(state.event === "init" || state.event === "no_enter") && (
-          <span className="message font-bold text-gray-400">
+          <span className="message font-bold text-gray-400 dark:text-gray-300">
             Type a keyword to search for results
           </span>
         )}
         {state.event === "no_result" && (
-          <span className="message font-bold text-red-700">
+          <span className="message font-bold text-red-700 dark:text-red-400">
             There are no results matching your search
           </span>
         )}
         {state.event === "done" && (
           <>
-            <span className="message">
+            <span className="message dark:text-blue-300">
               <b>{state.res.length}</b> results are listed for your search
             </span>
             <div className="flex flex-col gap-4 pt-3 pb-16" ref={parent}>
               {state.res.map((block, index) => (
                 <div
                   key={index}
-                  className="border rounded py-2 px-4 items-center profile-card"
+                  className="border rounded py-2 px-4 items-center profile-card dark:border-gray-700"
                 >
-                  <div className="border rounded-xl overflow-hidden h-[64px]">
+                  <div className="border rounded-xl overflow-hidden h-[64px] dark:border-gray-700">
                     <LazyLoadImage
                       alt={`${block.name.split(" ")[0]}'s profile picture`}
                       src={`https://picsum.photos/64/64?random=${index}`}
@@ -130,23 +130,24 @@ const AutoComplete = ({ data, keys = [] }) => {
                   </div>
                   <div className="flex flex-col justify-between text-left gap-3">
                     <div className="flex flex-col">
-                      <h3 className="text-slate-800 text-sm tracking-tighter font-semibold">
+                      <h3 className="text-slate-800 dark:text-slate-300 text-sm tracking-tighter font-semibold">
                         {block.name}
                       </h3>
-                      <span className="text-[10px] leading-3 text-gray-500">
+                      <span className="text-[10px] leading-3 text-gray-500 dark:text-slate-400">
                         @{block.username}
                       </span>
                     </div>
                     <div className="flex flex-col">
                       <a
                         href={`mailto:${block.email}`}
-                        className="flex items-center justify-start gap-1.5 text-slate-600 text-[11px] leading-4 hover:text-indigo-700 hover:underline"
+                        className="flex items-center justify-start gap-1.5 text-slate-600 dark:text-slate-500 text-[11px] leading-4 hover:text-indigo-700 dark:hover:text-indigo-300 hover:underline"
                       >
                         <FiMail /> {block.email}
                       </a>
                       <a
-                        href={block.website}
-                        className="flex items-center justify-start gap-1.5 text-slate-600 text-[11px] leading-4 hover:text-indigo-700 hover:underline"
+                        href={`https://${block.website}`}
+                        target="_blank"
+                        className="flex items-center justify-start gap-1.5 text-slate-600 dark:text-slate-500 text-[11px] leading-4 hover:text-indigo-700 dark:hover:text-indigo-300 hover:underline"
                       >
                         <FiLink2 /> {block.website}
                       </a>
